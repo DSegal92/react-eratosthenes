493d1cdaab8de815b47fbf6b82440276798cff22
Daniel Segal <dsegal92@gmail.com>
2016-04-18 21:38:31 -0400

    Minimal server for returning largest integer to test for primality

---
+++ server.rb
@@ -0,0 +1,13 @@
+require 'webrick'
+require 'json'
+
+root = File.expand_path './public'
+server = WEBrick::HTTPServer.new Port: 3000
+
+server.mount_proc '/sieve' do |req, res|
+  res.body = JSON.generate(JSON.parse(File.read('./sieve.json', encoding: 'UTF-8')))
+end
+
+trap('INT') { server.shutdown }
+
+server.start

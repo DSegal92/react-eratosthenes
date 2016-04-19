c3894e9788fc78d136500846535e58988d5afd0f
Daniel Segal <dsegal92@gmail.com>
2016-04-18 21:42:19 -0400

    Specify content as JSON and allow cross origin requests

--- server.rb
+++ server.rb
@@ -5,6 +5,9 @@ root = File.expand_path './public'
 server = WEBrick::HTTPServer.new Port: 3000
 
 server.mount_proc '/sieve' do |req, res|
+
+  res['Content-Type'] = 'application/json'
+  res['Access-Control-Allow-Origin'] = '*'
   res.body = JSON.generate(JSON.parse(File.read('./sieve.json', encoding: 'UTF-8')))
 end
 
